5e5d4d35913f391cabdbf268660aa4a861dfe627
Rakibul Islam <rakib1987@gmail.com>
2015-02-15 21:44:11 -0500

    initial commit pi_list

---
+++ Assignment_1_Solution/algo.rb
@@ -0,0 +1,15 @@
+class Algo
+  attr_accessor :pi_list
+
+  def initialize
+    @pi_list = []
+  end
+
+  def generate_pi_list
+    # write your algo to generate pi_list....
+    puts 'generating prime implicants....'
+  end
+
+  algo = Algo.new
+  algo.generate_pi_list
+end
