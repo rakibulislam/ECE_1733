df112bc83846b5572f7d92086ff1cce5662c3478
Rakibul Islam <rakib1987@gmail.com>
2015-02-16 01:29:57 -0500

    cleanup star_operation

--- Assignment_1_Solution/star_operation.rb
+++ Assignment_1_Solution/star_operation.rb
@@ -1,6 +1,6 @@
 require './starter_kit'
 class StarOperation
-def star(d1, d2)
+  def star(d1, d2)
     if d1 == '0' and d2 == '0'
       return '0'
     elsif d1 == '1' and d2 == '1'
@@ -14,39 +14,28 @@ def star(d1, d2)
     elsif (d1 == '1' and d2 == 'x') || (d1 == 'x' and d2 == '1')
       return '1'
     end
-end
+  end
 
-  def star_operation(cube_1, cube_2)
+  def star_operation(cube_A, cube_B)
     result = ''
-    retChar = ''
-    countNull = 0
-     puts cube_1.inspect
-     puts cube_2.inspect
+    number_of_nulls = 0
 
-    (0...cube_1.length).each do |index|
-      # puts star(cube_1[index], cube_2[index])
-      retChar = star(cube_1[index], cube_2[index])
+    (0...cube_A.length).each do |index|
+      star_of_two_chars = star(cube_A[index], cube_B[index])
       
-      if (retChar == 'n')
-        countNull += 1
-        
-        if countNull == 1
+      if (star_of_two_chars == 'n')
+        number_of_nulls += 1
+        if number_of_nulls == 1
           # for just one null, the null becomes x         
-          result +='x'  
-        elsif countNull > 1
+          result += 'x'
+        elsif number_of_nulls > 1
           # for more than one null, the star-operation result becomes null
-          result = 'null'
-          break
+          return 'NULL'
         end
-        
-        # return result
-        
       else
-        # bitwise operation result is not null
-        result += retChar
-        
+        # star operation result is not null
+        result += star_of_two_chars
       end
-      
     end    
     
     result
@@ -61,15 +50,16 @@ end
   puts "DC_SET: #{starter_kit.dc_set}"
   puts "all cubes: #{starter_kit.cubes}"
   puts "Function cost: #{starter_kit.function_cost(starter_kit.cubes)}"
-  puts '----------------------------------------------------'
-  
-  
-  # puts 'Some example star operation:'
+  puts '- - - - - - - - - - - - - - - - - - - - - - - - - '
+  puts '- - - - - - - - - - - - - - - - - - - - - - - - - '
   star = StarOperation.new
   puts "#{starter_kit.cubes[0]} * #{starter_kit.cubes[1]}: #{star.star_operation(starter_kit.cubes[0], starter_kit.cubes[1])}"
-  #puts "#{starter_kit.cubes[0]} * #{starter_kit.cubes[2]}: #{star.star_operation(starter_kit.cubes[0], starter_kit.cubes[2])}"
-  #puts "#{starter_kit.cubes[0]} * #{starter_kit.cubes[3]}: #{star.star_operation(starter_kit.cubes[0], starter_kit.cubes[3])}"
-  #puts "#{starter_kit.cubes[1]} * #{starter_kit.cubes[3]}: #{star.star_operation(starter_kit.cubes[1], starter_kit.cubes[3])}"
-  
-  
+
+  cube_A = '0x0'
+  cube_B = '1x1'
+  puts "Example 1: #{cube_A} # #{cube_B} = #{star.star_operation(cube_A, cube_B)}"
+
+  cube_A = '0x0'
+  cube_B = 'x11'
+  puts "Example 2: #{cube_A} # #{cube_B} = #{star.star_operation(cube_A, cube_B)}"
 end
